4b9eb20f7a2e209c1e0e80c43968eeccb158dee4
Sistemi Ufficio <168089250+SistemiUfficio@users.noreply.github.com>
2024-05-15 15:46:44 +0200

    modifiche fatte al 15/05/2024

--- Program.cs
+++ Program.cs
@@ -1,17 +1,22 @@
 ﻿// memorizzazione di 50 numeri casuali in un array 
 
 
-int max_numeri = 10;
+using System;
+
+int max_numeri = 20;
 
 
 
 Random r = new Random();
 int[] casuali = new int[max_numeri];
-
-for (int i = 0; i < max_numeri; i++) casuali[i] = r.Next(50);
-
+for (int i = 0; i < max_numeri; i++)
+{
+    casuali[i] = r.Next(10);
+}
 int minimo = casuali[0];
 int massimo = casuali[0];
+
+
 
 foreach (int num in casuali)
 {
@@ -46,3 +51,76 @@ foreach (int n in casuali)
 Console.WriteLine($"La somma è: {somma}");
 
 //posizione di un elemento cercato 
+Console.WriteLine("inserisci valore da ricercare");
+
+string valore = Console.ReadLine();
+int contatore = 0;
+
+if (int.TryParse(valore, out int valoreintero))
+
+{
+
+    for (int i = 0; i < max_numeri; i++)
+    {
+
+        if (casuali[i].ToString() == valore)
+        {
+            Console.WriteLine($"Il valore che cercavi si trova alla posizione: {i + 1}");
+            contatore++;
+
+        }
+    }
+}
+ else
+{ Console.WriteLine("Trovato dei caratteri diversi da numeri"); }
+
+
+Console.WriteLine($"Il valore è comparso: {contatore} volte");
+
+
+// Inserire o eliminare elementi dalla lista
+// Per esempio, aggiungiamo un nuovo numero alla fine della lista
+//casuali.Add(42);
+
+// O eliminiamo il primo elemento della lista
+//casuali.RemoveAt(0);
+
+// E così via...
+
+
+Console.WriteLine("aggiungere un record? scrivi si se vuoi continuare");
+string risposta= Console.ReadLine();
+
+if (risposta=="si")
+{
+    Array.Resize(ref casuali, max_numeri + 1);
+
+    Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} ");
+    Console.WriteLine("inserisci un valore intero:");
+    int numeroins = int.Parse (Console.ReadLine());
+
+    casuali[max_numeri] = numeroins;
+}
+
+Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} ");
+
+Console.WriteLine("eliminare un record? scrivi si se vuoi continuare");
+string risposta2 = Console.ReadLine();
+
+if (risposta2 == "si")
+{
+    Array.Resize(ref casuali, max_numeri - 1);
+
+
+    Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} " );
+}
+
+
+
+//inserimento o eliminazione di un array 
+
+
+
+
+
+
