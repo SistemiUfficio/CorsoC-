687eff4188c9502377eb83d85277ca16b9ab51ea
Matteo <m.scalabrin@sistemi-ufficio.it>
2024-05-22 00:01:34 +0200

    21/05/2024

--- Program.cs
+++ Program.cs
@@ -1,126 +1,149 @@
-﻿// memorizzazione di 50 numeri casuali in un array 
+﻿// See https://aka.ms/new-console-template for more information
+// creare un programma che mi vada attraverso l'uso di una funzione, a calcolare la rata di scadenza del mago, dato le rate in input della condizione di pagamento 
 
 
-using System;
+//funzione calcola rata 
+//convertire data in stringa  = DateTime.Parse(dataStringa)
+//DateTime nuovaData = dataIniziale.AddDays(giorniDaAggiungere);
+using System.Globalization;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-int max_numeri = 20;
-
-
-
-Random r = new Random();
-int[] casuali = new int[max_numeri];
-for (int i = 0; i < max_numeri; i++)
+static string CalcolaRate(DateTime datarata, int giorni, int rate )
 {
-    casuali[i] = r.Next(10);
-}
-int minimo = casuali[0];
-int massimo = casuali[0];
 
+    int calcologiorni = giorni * rate;
 
+        DateTime nuovaData = datarata.AddDays(calcologiorni - 1);
 
-foreach (int num in casuali)
-{
-    Console.WriteLine(num);
-
-
-    //TROVO IL MASSIMO E IL MINIMO 
-    if (minimo >= num)
-    {
-        minimo = num;
-    }
-    else if (massimo <= num) {
-
-        massimo = num;
-    }
+        return nuovaData.ToString("dd/MM/yyyy");
     
+
+
 }
 
-Console.WriteLine($"valore minimo {minimo} e valore massimo {massimo}");
 
-
-
-//sommatoria
-
-long somma = 0;
-
-foreach (int n in casuali)
+//funzione per rilevamento errore 
+static string Errore(bool esito, int blocco)
 {
-    somma += n;
+    string errore = $"Hai inserito un valore non valido nel blocco {blocco}. Rilancia il programma";
+    return errore;
 
 }
-Console.WriteLine($"La somma è: {somma}");
 
-//posizione di un elemento cercato 
-Console.WriteLine("inserisci valore da ricercare");
 
-string valore = Console.ReadLine();
-int contatore = 0;
+//input condizione di pagamento 
+try
+{
+    Console.WriteLine("inserisci data della fattura per calcolare la rata del pagamento:");
+    Console.Write("Giorno: ");
+    int giorno = int.Parse(Console.ReadLine());
+    Console.Write("Mese: ");
+    int mese = int.Parse(Console.ReadLine());
+    Console.Write("Anno : ");
+    int anno = int.Parse(Console.ReadLine());
 
-if (int.TryParse(valore, out int valoreintero))
+    
+
+    Console.WriteLine($"{Environment.NewLine}");
+
+    DateTime datainiziale = new DateTime(anno, mese, giorno);
+    Console.Write("Numero di rate: ");
+    int nrate = int.Parse(Console.ReadLine());
+
+    Console.Write("Giorni di distanza tra le rate: ");
+    int giorniagg = int.Parse(Console.ReadLine());
+
+
+
+    //scrivo il risultato 
+    for (int i = 1; i < nrate + 1; i++)
+        Console.WriteLine($"Rata {i}: {CalcolaRate(datainiziale, giorniagg, i)}");
+}
+catch
+{
+    int blocco = 0;
+    bool esitocatch = true;
+    Console.WriteLine($"{Errore(esitocatch,blocco)}");
+
+}
+
+
+
+Console.WriteLine($"{Environment.NewLine}");
+
+//creare una matrice con le fatture , data inserimento delle fatture , giorni da calcolare 
+//blocco1
+Console.Write("Numero delle fatture:");
+int nrfatture1 = int.Parse(Console.ReadLine());
+
+int[] Fatture = new int[nrfatture1];  //NRDELLE FATTURE 
+
+
+
+Console.Write("Numero di rate: ");
+int nrate1 = int.Parse(Console.ReadLine());  //NUMERO DELLE RATE 
+int[] Nrrate1 = new int[nrate1];
+
+string[,] rate = new string[nrfatture1, nrate1];  //MATRICE 
+
+Console.WriteLine($"{Environment.NewLine}");
+Console.Write("Giorni di distanza tra le rate: "); //DISTANZA TRA LE RATE 
+int giorniagg1 = int.Parse(Console.ReadLine());
+
+
+
+
+//STAMPO LE FATTURE CON LA MATRICE 
+//Console.WriteLine("inserisci data della fattura per calcolare la rata del pagamento:");
+//Console.Write("Giorno: ");
+//int giorno1 = int.Parse(Console.ReadLine());
+//Console.Write("Mese: ");
+//int mese1 = int.Parse(Console.ReadLine());
+//Console.Write("Anno : ");
+//int anno1 = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine($"inserisci data fattura nr {i}");
+string dataInput = Console.ReadLine();
+
+
+//DateTime datainiziale1 = new DateTime(anno1, mese1, giorno1);  // DATA DI PARTENZA 
+
+//STAMPO LE RATE 
+Console.WriteLine($"{Environment.NewLine}");
+for (int i = 1; i < Nrrate1.Length + 1; i++) Console.Write($"\t\t\tRata:{i}");
+
+
+
+for (int i = 1;i < rate .GetLength (0) +1; i++) //stampo matrice
+
+
+
+
+
+// Tentativo di parsing della data
+DateTime dataIniziale;
+bool isDataValida = DateTime.TryParseExact(dataInput, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataIniziale);
+
+Console.WriteLine($"{dataIniziale}");
+
 
 {
+    Console.WriteLine();
+    Console.Write($"Fattura nr:{i.ToString().PadRight (5,' ')}");
+  for (int j = 1; j < rate.GetLength (1)+1; j++)
+  {
 
-    for (int i = 0; i < max_numeri; i++)
-    {
+        Console.Write($"\t\t{CalcolaRate(datainiziale1,giorniagg1 ,j)}");
 
-        if (casuali[i].ToString() == valore)
-        {
-            Console.WriteLine($"Il valore che cercavi si trova alla posizione: {i + 1}");
-            contatore++;
 
-        }
     }
-}
- else
-{ Console.WriteLine("Trovato dei caratteri diversi da numeri"); }
 
 
-Console.WriteLine($"Il valore è comparso: {contatore} volte");
-
-
-// Inserire o eliminare elementi dalla lista
-// Per esempio, aggiungiamo un nuovo numero alla fine della lista
-//casuali.Add(42);
-
-// O eliminiamo il primo elemento della lista
-//casuali.RemoveAt(0);
-
-// E così via...
-
-
-Console.WriteLine("aggiungere un record? scrivi si se vuoi continuare");
-string risposta= Console.ReadLine();
-
-if (risposta=="si")
-{
-    Array.Resize(ref casuali, max_numeri + 1);
-
-    Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} ");
-    Console.WriteLine("inserisci un valore intero:");
-    int numeroins = int.Parse (Console.ReadLine());
-
-    casuali[max_numeri] = numeroins;
-}
-
-Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} ");
-
-Console.WriteLine("eliminare un record? scrivi si se vuoi continuare");
-string risposta2 = Console.ReadLine();
-
-if (risposta2 == "si")
-{
-    Array.Resize(ref casuali, max_numeri - 1);
-
-
-    Console.WriteLine($"Ora il numero toale di valori nell'array è di: {max_numeri} " );
 }
 
 
-
-//inserimento o eliminazione di un array 
-
-
-
-
-
+Console.WriteLine($"{Environment.NewLine}");
 
